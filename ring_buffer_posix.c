945b5ad3f1022a8e4c7c1d983ec7555dab37a1de
UserIsNotAvailable <87370521+UserIsNotAvailable@users.noreply.github.com>
2024-10-06 23:03:15 +0900

    Update ring_buffer_posix.c
    move data from heap to stack.

--- ring_buffer_posix.c
+++ ring_buffer_posix.c
@@ -10,10 +10,10 @@ typedef struct blocking_queue {
     size_t capacity;
     size_t head;
     size_t tail;
-    pthread_mutex_t *mutex_head;
-    pthread_mutex_t *mutex_tail;
-    sem_t *sem_element;
-    sem_t *sem_space;
+    pthread_mutex_t mutex_head;
+    pthread_mutex_t mutex_tail;
+    sem_t sem_element;
+    sem_t sem_space;
 } blocking_queue;
 
 void blocking_queue_init(blocking_queue *const bq, const size_t capacity) {
@@ -21,46 +21,38 @@ void blocking_queue_init(blocking_queue *const bq, const size_t capacity) {
     bq->capacity = capacity;
     bq->head = 0;
     bq->tail = 0;
-    bq->mutex_head = malloc(sizeof(pthread_mutex_t));
-    bq->mutex_tail = malloc(sizeof(pthread_mutex_t));
-    bq->sem_element = malloc(sizeof(sem_t));
-    bq->sem_space = malloc(sizeof(sem_t));
-    pthread_mutex_init(bq->mutex_head, NULL);
-    pthread_mutex_init(bq->mutex_tail, NULL);
-    sem_init(bq->sem_element, 0, 0);
-    sem_init(bq->sem_space, 0, capacity);
+    pthread_mutex_init(&bq->mutex_head, NULL);
+    pthread_mutex_init(&bq->mutex_tail, NULL);
+    sem_init(&bq->sem_element, 0, 0);
+    sem_init(&bq->sem_space, 0, capacity);
 }
 
-void blocking_queue_destroy(const blocking_queue *const bq) {
-    sem_destroy(bq->sem_space);
-    sem_destroy(bq->sem_element);
-    pthread_mutex_destroy(bq->mutex_tail);
-    pthread_mutex_destroy(bq->mutex_head);
-    free(bq->sem_space);
-    free(bq->sem_element);
-    free(bq->mutex_tail);
-    free(bq->mutex_head);
+void blocking_queue_destroy(blocking_queue *const bq) {
+    sem_destroy(&bq->sem_space);
+    sem_destroy(&bq->sem_element);
+    pthread_mutex_destroy(&bq->mutex_tail);
+    pthread_mutex_destroy(&bq->mutex_head);
     free(bq->buffer);
 }
 
 void blocking_queue_put(blocking_queue *const bq, const void *const e) {
-    sem_wait(bq->sem_space);
-    pthread_mutex_lock(bq->mutex_tail);
+    sem_wait(&bq->sem_space);
+    pthread_mutex_lock(&bq->mutex_tail);
     bq->buffer[bq->tail] = e;
     printf("%d %p -> [%lu]\n", gettid(), e, bq->tail);
     bq->tail = (bq->tail + 1) % bq->capacity;
-    pthread_mutex_unlock(bq->mutex_tail);
-    sem_post(bq->sem_element);
+    pthread_mutex_unlock(&bq->mutex_tail);
+    sem_post(&bq->sem_element);
 }
 
 const void *blocking_queue_take(blocking_queue *const bq) {
-    sem_wait(bq->sem_element);
-    pthread_mutex_lock(bq->mutex_head);
+    sem_wait(&bq->sem_element);
+    pthread_mutex_lock(&bq->mutex_head);
     const void *e = bq->buffer[bq->head];
     printf("%d     [%lu] -> %p\n", gettid(), bq->head, e);
     bq->head = (bq->head + 1) % bq->capacity;
-    pthread_mutex_unlock(bq->mutex_head);
-    sem_post(bq->sem_space);
+    pthread_mutex_unlock(&bq->mutex_head);
+    sem_post(&bq->sem_space);
     return e;
 }
 
